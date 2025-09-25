e0251bdde434540706e4210603e8d503dab21ba4
Abhi wargad <Wargadabhi@gmail.com>
2025-09-25 12:21:39 +0530

    Add files via upload

---
+++ AddSubMulDivd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+        printf("Enter two numbers:");
+        scanf("%d %d",&a,&b);
+
+    printf("Addition %d\n",a+b);
+    printf("Substraction %d\n",a-b);
+    printf("multiplication %d\n",a*b);
+    printf("divide %d\n",a/b);
+
+return 0;
+}
---
+++ two integer and display.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+        printf("Enter two integer:");
+        scanf("%d %d",&a,&b);
+         
+        printf("First integer %d\n",a);
+        printf("second integer %d\n",b);
+    return 0;
+
+}
