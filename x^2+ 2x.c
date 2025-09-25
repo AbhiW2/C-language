17df9b39390dbcd4a24d443f5029f9903e25f676
Abhi wargad <Wargadabhi@gmail.com>
2025-09-25 22:02:45 +0530

    Add files via upload

---
+++ x^2+ 2x.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+    int x,result;
+        printf("Enter a number:");
+        scanf("%d",&x);
+
+    result=x*x+2*x;
+
+        printf("Result: %d\n",result);
+return 0;
+
+}
---
+++ x^2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int x,result;
+        printf("Enter a number:");
+        scanf("%d",&x);
+
+    result=x*x;
+
+        printf("Result %d\n",result);
+
+return 0;
+}
