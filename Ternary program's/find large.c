0f43016dbc8079595de7c0b98dff7fbd49495b7d
Abhi wargad <Wargadabhi@gmail.com>
2025-09-30 16:10:27 +0530

    Add files via upload

---
+++ Ternary program's/find large.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+        printf("Enter two number:");
+        scanf("%d%d",&a,&b);
+
+    (a>b)? printf("Largest number:%d",a):printf("Largest number:%d",b);
+
+    return 0;
+}
---
+++ Ternary program's/pass fail.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int marks;
+        printf("Enter marks:");
+        scanf("%d",&marks);
+
+    (marks>=40)?printf("pass"):printf("fail");
+
+return 0;
+}
