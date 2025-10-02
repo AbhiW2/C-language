972e15310465e0b6258248fe319c03566740342a
Abhi wargad <Wargadabhi@gmail.com>
2025-10-02 18:24:28 +0530

    Add files via upload

---
+++ Test 2/Q3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+        printf("Enter a number:");
+        scanf("%d",&n);
+
+    int i=n;
+
+        while(i>=1){
+            printf("%d-",i);
+            i--;
+        }
+        return 0;
+}
