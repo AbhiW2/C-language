dddb6e6b8158055f0c56f114321ed6edc67bd517
Abhi wargad <Wargadabhi@gmail.com>
2025-10-02 18:24:08 +0530

    Add files via upload

---
+++ Test 2/Q2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+        printf("Enter two numbers:");
+        scanf("%d %d",&a,&b);
+    
+    (a>b)? printf("Number a is larger=%d"):printf(" Number b is larger=%d");
+
+    return 0;
+}
