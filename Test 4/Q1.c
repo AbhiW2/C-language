02f3b2d4f5bf5a17301bf94dc5f20eddff89a7ac
Abhi wargad <Wargadabhi@gmail.com>
2025-10-04 19:06:04 +0530

    Add files via upload

---
+++ Test 4/Q1.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int n;
+        printf("Enter number of array:");
+        scanf("%d",&n);
+
+        int*arr=(int*)malloc(n*sizeof(int));
+
+        printf("Enter %d elements in array:",n);
+    int i;
+        for(i=0;i<n;i++){
+            scanf("%d",&arr[i]);
+        }
+        int pos=0,neg=0,zero=0;
+            for(i=0;i<n;i++){
+                if(arr[i]>0)
+                    pos++;
+                    else if(arr[i]<0)
+                        neg++;
+                        else
+                        zero++;
+            }
+            printf("Positive count:%d\n",pos);
+            printf("Negative count:%d\n",neg);
+            printf("Zero count:%d\n",zero);
+
+            int j;
+                printf("Pais with positive sum:");
+                    for(i=0;i<n;i++){
+                        for(j=i+1;j<n;j++)
+                            if(arr[i]+arr[j]>0)
+
+            printf("(%d %d)\n",arr[i],arr[j]);
+                    }
+                    free(arr);
+                    return 0;
+}
