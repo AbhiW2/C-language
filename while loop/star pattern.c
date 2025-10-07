a195d61be7bbf33be0bb2c27902c26ad4143ecde
Abhi wargad <Wargadabhi@gmail.com>
2025-10-07 09:42:22 +0530

    Add files via upload

---
+++ while loop/star pattern.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+        printf("Enter a number:");
+        scanf("%d",&n);
+    int i=1;
+        while(i<=n){
+            int j=1;
+                while(j<=n){
+                    printf("* ");
+                    j++;
+                }
+                printf("\n");
+                i++;
+        }
+        
+        return 0;
+}
