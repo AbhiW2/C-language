178c7aee2c857e28d0459678489217ae09170afe
Abhi wargad <Wargadabhi@gmail.com>
2025-10-15 10:02:13 +0530

    Add files via upload

---
+++ Use of break and continue/sum until user enter neg num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int num,sum=0;
+       ;
+    while(1){
+         printf("Enter a number:");
+        scanf("%d",&num);
+        if(num<0)
+        break;
+        sum+=num;
+        num++;
+    }
+    printf("sum:%d\n",sum);
+return 0;
+}
