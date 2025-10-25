14ae5379f67e4c23ca2089772f83d386a590133e
Abhi wargad <Wargadabhi@gmail.com>
2025-10-25 18:56:03 +0530

    Create Read character and print it.c

---
+++ Read character and print it.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char ch[1]; 
+    
+    printf("Enter a character: ");
+    scanf("%s", ch);  
+    
+    printf("You entered: %s\n", ch);
+    
+    return 0;
+}
