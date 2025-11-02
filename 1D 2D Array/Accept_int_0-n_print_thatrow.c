e0462095cd9c5944b34f791b6b160ad8a2d414d1
AbhiW2 <wargadabhi@gmail.com>
2025-11-02 20:59:42 +0530

    print row

---
+++ 1D 2D Array/Accept_int_0-n_print_thatrow.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int n;
+        printf("Enter a size of matrix:");
+        scanf("%d",&n);
+    int A[n][n];
+        printf("Enter %d elements in array:",n*n);
+    int r,c;
+            for(r=0;r<n;r++)
+                for(c=0;c<n;c++){
+                    scanf("%d",&A[r][c]);
+                }
+            printf("Enter row number(0 to %d):",n-1);
+            scanf("%d",&r);
+                if(r>=0 && r<n){
+                    printf("Row %d:",r);
+                for(c=0;c<n;c++){
+                    printf("%d ",A[r][c]);
+                }
+            }
+                else
+                    printf("Invalid row enter");
+
+        return 0;
+}
