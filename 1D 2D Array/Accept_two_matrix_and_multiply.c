5caa7d6f832dc31feb3bffea4ebc6e7bef94b09c
Abhi Wargad <wargadabhi@gmail.com>
2025-11-14 11:29:39 +0530

    Multiplication of two matrix

---
+++ 1D 2D Array/Accept_two_matrix_and_multiply.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main(){
+    int n;
+        printf("Enter size of matrix:");
+        scanf("%d",&n);
+    int A[n][n];
+    int B[n][n];
+        printf("Enter %d elements in A matrix:\n",n*n);
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&A[i][j]);
+        }
+    }
+    printf("Enter %d elements in B matrix:\n",n*n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&B[i][j]);
+        }
+    }
+int result[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                result[i][j]=(A[i][j]*B[i][j]);
+            }
+        }
+        printf("Multiplication of two matrix:\n");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                printf("%d ",result[i][j]);
+            }
+        printf("\n");
+        }
+    
+return 0;
+}
