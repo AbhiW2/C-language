e8194ccf9cf4eb072fa45dab77f6c46e756a3bc4
Abhi Wargad <wargadabhi@gmail.com>
2025-11-21 15:52:37 +0530

    Find maximum

---
+++ Home Assignment/Find_max_using_ternary.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+        printf("Enter a two number:");
+        scanf("%d%d",&a,&b);
+    (a>b)? printf("%d A is maximum",a):printf("%d B is maximum",b);
+
+    return 0;
+}
