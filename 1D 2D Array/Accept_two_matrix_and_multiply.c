5d958d69f15f187e42ba79a7cc9dc38a9b2004a9
Abhi Wargad <wargadabhi@gmail.com>
2025-11-28 18:45:39 +0530

    copy

--- 1D 2D Array/Accept_two_matrix_and_multiply.c
+++ 1D 2D Array/Accept_two_matrix_and_multiply.c
@@ -5,20 +5,20 @@ int main(){
         scanf("%d",&n);
     int A[n][n];
     int B[n][n];
-        printf("Enter %d elements in A matrix:\n",n*n);
-    
+
+  printf("Enter %d elements in A matrix:\n",n*n);
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             scanf("%d",&A[i][j]);
         }
     }
-    printf("Enter %d elements in B matrix:\n",n*n);
+  printf("Enter %d elements in B matrix:\n",n*n);
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             scanf("%d",&B[i][j]);
         }
     }
-int result[n][n];
+    int result[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 result[i][j]=(A[i][j]*B[i][j]);
