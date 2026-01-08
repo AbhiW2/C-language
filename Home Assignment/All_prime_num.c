07c1f5aec1bbe26241a47fa06c129b75697784bb
Abhi Wargad <wargadabhi@gmail.com>
2026-01-08 08:44:10 +0530

    All prime

---
+++ Home Assignment/All_prime_num.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, isPrime;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Prime numbers between 2 and %d are:\n", n);
+
+    for (i = 2; i <= n; i++) {
+        isPrime = 1;   // Assume number is prime
+
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                isPrime = 0;  // Not prime
+                break;
+            }
+        }
+
+        if (isPrime == 1) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
